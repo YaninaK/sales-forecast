fd4bcfa1b8dc09ca601b223f9abec17439cde554
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-02-22 15:36:59 +0300

    Fix johnson_su_transformation.py

--- src/sales_forecast/data/johnson_su_transformation.py
+++ src/sales_forecast/data/johnson_su_transformation.py
@@ -1,6 +1,14 @@
+import logging
 import pandas as pd
 import math
 import scipy.stats as st
+from typing import Optional
+
+__all__ = ["Johnson_SU_transformation"]
+
+logger = logging.getLogger()
+
+N_SHOPS = 20
 
 
 class JohnsonSU:
@@ -9,8 +17,12 @@ class JohnsonSU:
     https://en.wikipedia.org/wiki/Johnson%27s_SU-distribution
     """
 
-    def __init__(self):
+    def __init__(self, n_shops: Optional[int] = None):
+        if n_shops is None:
+            n_shops = N_SHOPS
+
         self.johnsonsu_params = dict()
+        self.n_shops = n_shops
 
     def johnsonsu_transform(self, x, a, b, loc, scale):
         return a + b * math.asinh((x - loc) / scale)
