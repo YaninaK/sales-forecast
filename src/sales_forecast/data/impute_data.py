9ef62ac62ca8ad95965f7ad4d6ab14877e87973d
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-02-23 04:59:17 +0300

    Fix impute_data.py

--- src/sales_forecast/data/impute_data.py
+++ src/sales_forecast/data/impute_data.py
@@ -9,45 +9,56 @@ __all__ = ["impute_data"]
 logger = logging.getLogger()
 
 
-def impute(data: pd.DataFrame) -> pd.DataFrame:
+IMPUTE_DATE_SHOPS = [
+    [
+        ["2016-06-19", "2017-06-04"],
+        [2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 16, 17, 18, 19],
+    ],
+    [["2016-01-05", "2017-01-06"], [4, 8, 10, 12, 13, 17, 18]],
+    [["2016-11-01"], [7]],
+]
 
-    logging.info("Imputing data...")
-
-    n_shops = data.shape[1]
-    missing_dates = [pd.to_datetime(t) for t in ["2016-06-19", "2017-06-04"]]
-    cols = [i for i in range(n_shops) if not i in [0, 1, 11, 12, 15]]
-    data = impute_with_medians(missing_dates, cols=cols, df=data)
-
-    missing_dates = [pd.to_datetime(t) for t in ["2016-01-05", "2017-01-06"]]
-    cols = [i for i in [4, 8, 10, 12, 13, 17, 18]]
-    data = impute_with_medians(missing_dates, cols=cols, df=data)
-
-    missing_dates = [pd.to_datetime("2016-11-01")]
-    data = impute_with_medians(missing_dates, cols=[7], df=data)
-
-    correlated_shops = {
+CORRELATED_SHOPS = {
+    0: {
         3: [0, 1, 15, 19],
         11: [0, 1, 3, 15, 19],
         12: [0, 1, 3, 11, 15, 19],
         14: [0, 1, 3, 11, 12, 15, 19],
         18: [0, 1, 3, 11, 12, 14, 15, 19],
-    }
-    data = impute_by_regression(correlated_shops, data)
+    },
+    1: {2: [7, 9, 16]},
+}
 
-    correlated_shops = {2: [7, 9, 16]}
-    data = impute_by_regression(correlated_shops, data)
+
+def impute(
+    data: pd.DataFrame,
+    impute_dates_shops: Optional[list] = None,
+    correlated_shops: Optional[dict] = None,
+) -> pd.DataFrame:
+
+    if impute_dates_shops is None:
+        impute_dates_shops = IMPUTE_DATE_SHOPS
+    if correlated_shops is None:
+        correlated_shops = CORRELATED_SHOPS
+
+    for missing_dates, shops in impute_dates_shops:
+        data = impute_with_medians(missing_dates, shops, data)
+
+    for i in range(len(correlated_shops)):
+        data = impute_by_regression(correlated_shops[i], data)
 
     return data
 
 
 def impute_with_medians(
-    missing_dates: list, cols: list, df: pd.DataFrame
+    missing_dates, cols, df: pd.DataFrame
 ) -> pd.DataFrame:
     """
     Imputes values in missing_dates with medians for particular year-month-weekday
     """
     logging.info("Imputeing missing values with medians...")
 
+    missing_dates = [pd.to_datetime(t) for t in missing_dates]
     medians = pd.DataFrame(index=cols, columns=missing_dates)
     for i in missing_dates:
         cond_1 = df.index.year == i.year
